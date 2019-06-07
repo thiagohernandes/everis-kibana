6151a136a34d45a190effde651767eb2eaa5cbc6
Thiago Hernandes Souza <thernans@everis.com>
2019-06-07 09:13:50 -0300

    Criação do projeto

--- src/main/java/com/everis/kibana/AppMainEveris.java
+++ src/main/java/com/everis/kibana/AppMainEveris.java
@@ -3,11 +3,11 @@ package com.everis.kibana;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EverisKibanaApplication {
+@SpringBootApplication(scanBasePackages = { "com.everis.kibana" })
+public class AppMainEveris {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EverisKibanaApplication.class, args);
+		SpringApplication.run(AppMainEveris.class, args);
 	}
 
 }
--- src/test/java/com/everis/kibana/AppMainEverisTests.java
+++ src/test/java/com/everis/kibana/AppMainEverisTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EverisKibanaApplicationTests {
+public class AppMainEverisTests {
 
 	@Test
 	public void contextLoads() {
